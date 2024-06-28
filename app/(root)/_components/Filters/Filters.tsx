8343afe5f30db0925fb3a99e772aa44d522d93ff
Dennis Polukaroff <dennispolukaroff@gmail.com>
2024-06-28 11:59:25 +0300

    add pagination component

--- app/(root)/_components/Filters/Filters.tsx
+++ app/(root)/_components/Filters/Filters.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import {
+	Box,
+	Button,
 	Combobox,
 	Fieldset,
 	Group,
@@ -9,51 +11,147 @@ import {
 	Pill,
 	PillsInput,
 	Select,
+	Text,
 	TextInput,
 	useCombobox,
 } from "@mantine/core";
 import styles from "./Filters.module.scss";
 import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import { BiExpandVertical } from "react-icons/bi";
 import { createReleaseYears } from "@/utils/createReleaseYears";
 
 const genres = ["Apples", "Bananas", "Broccoli", "Carrots", "Chocolate"];
+const ratingNumbers = [...Array(11)].map((_, i) => i.toString()).slice(1, 11);
+const ratingNumbersDesc = [...Array(11)]
+	.reverse()
+	.map((_, i) => i.toString())
+	.slice(1, 11);
+const sortByOptions = ["Most popular", "Rating", "Genre", "Year"];
 
 const Filters = () => {
 	const [year, setYear] = useState<string | null>("");
+	const [sortBy, setSortBy] = useState<string | null>("Most popular");
 
 	return (
 		<form>
-			<Group>
-				<MultiSelect
-					label='Genres'
-					placeholder='Select genre'
-					data={genres}
-					w={280}
-					comboboxProps={{
-						transitionProps: { transition: "pop", duration: 200 },
-						dropdownPadding: 0,
-						shadow: "md",
-					}}
-					rightSectionPointerEvents='none'
-					rightSection={<IoIosArrowDown />}
-					hidePickedOptions
-				/>
+			<Group
+				style={{ justifyContent: "space-between" }}
+				mt={"lg"}
+				mb={"xl"}>
+				<Box>
+					<Text
+						mb={"sm"}
+						c={"red.7"}>
+						Genres
+					</Text>
+					<MultiSelect
+						placeholder='Select genre'
+						data={genres}
+						w={280}
+						rightSectionPointerEvents='none'
+						rightSection={<IoIosArrowDown />}
+						hidePickedOptions
+						comboboxProps={{
+							transitionProps: { transition: "pop", duration: 200 },
+							dropdownPadding: 0,
+							shadow: "md",
+						}}
+					/>
+				</Box>
 
-				<Select
-					data={createReleaseYears()}
-					value={year}
-					onChange={setYear}
-					comboboxProps={{
-						transitionProps: { transition: "pop", duration: 200 },
-						dropdownPadding: 0,
-						shadow: "md",
-					}}
-					label='Release year'
-					placeholder='Select release year'
-					rightSectionPointerEvents='none'
-					rightSection={<IoIosArrowDown />}
-				/>
+				<Box>
+					<Text
+						mb={"sm"}
+						c={"red.7"}>
+						Release year
+					</Text>
+					<Select
+						data={createReleaseYears()}
+						value={year}
+						onChange={setYear}
+						w={200}
+						placeholder='Select release year'
+						rightSectionPointerEvents='none'
+						rightSection={<IoIosArrowDown />}
+						comboboxProps={{
+							transitionProps: { transition: "pop", duration: 200 },
+							dropdownPadding: 0,
+							shadow: "md",
+						}}
+					/>
+				</Box>
+
+				<Box>
+					<Text
+						mb={"sm"}
+						c={"red.7"}>
+						Ratings
+					</Text>
+					<Group>
+						<Select
+							data={ratingNumbers}
+							value={year}
+							onChange={setYear}
+							w={130}
+							placeholder='From'
+							rightSectionPointerEvents='none'
+							rightSection={<BiExpandVertical />}
+							comboboxProps={{
+								transitionProps: { transition: "pop", duration: 200 },
+								dropdownPadding: 0,
+								shadow: "md",
+							}}
+						/>
+
+						<Select
+							data={ratingNumbersDesc}
+							value={year}
+							onChange={setYear}
+							w={130}
+							placeholder='To'
+							rightSectionPointerEvents='none'
+							rightSection={<BiExpandVertical />}
+							comboboxProps={{
+								transitionProps: { transition: "pop", duration: 200 },
+								dropdownPadding: 0,
+								shadow: "md",
+							}}
+						/>
+					</Group>
+				</Box>
+
+				<Button
+					variant='subtle'
+					color='orange'
+					style={{ alignSelf: "flex-end" }}>
+					Reset filters
+				</Button>
+
+				<Box
+					style={{
+						marginLeft: "auto",
+					}}>
+					<Text
+						mb={"sm"}
+						c={"red.7"}>
+						Sort by
+					</Text>
+					<Select
+						data={sortByOptions}
+						value={sortBy}
+						onChange={setSortBy}
+						w={200}
+						defaultValue={sortBy}
+						rightSectionPointerEvents='none'
+						rightSection={<IoIosArrowDown />}
+						comboboxProps={{
+							transitionProps: { transition: "pop", duration: 200 },
+							dropdownPadding: 0,
+							shadow: "md",
+						}}
+					/>
+				</Box>
 			</Group>
 		</form>
 	);
--- components/PaginationMovies/PaginationMovies.tsx
+++ components/PaginationMovies/PaginationMovies.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Pagination } from "@mantine/core";
+import styles from "./PaginationMovies.module.scss";
 
 const PaginationMovies = () => {
-	return <div style={{ marginTop: "auto" }}>Pagination</div>;
+	return (
+		<div className={styles.paginationContainer}>
+			<Pagination
+				total={500}
+				siblings={3}
+				defaultValue={10}
+				color='brown.4'
+				className={styles.pagination}
+			/>
+		</div>
+	);
 };
 
 export default PaginationMovies;
